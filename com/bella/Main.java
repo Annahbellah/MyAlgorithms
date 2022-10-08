046cc3bdbe0dfb3312b1b8998ecdaded91d94097
Anne Agbo <agboanneananwune@gmail.com>
2022-10-08 16:30:18 +0100

    daily algorithm

--- com/bella/Main.java
+++ com/bella/Main.java
@@ -1,7 +1,18 @@
 package com.bella;
 
 public class Main {
-    public static void main(String[] args) {
-    int var = 23;
+    // write a function that returns the minimum number in an array.
+    static int min(int arr[]) {
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+        }
+
+        return min;
     }
-}
+
+    }
+
+
